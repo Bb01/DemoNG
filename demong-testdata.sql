30356ba5de3a38b99674f18a3ba755d9ff142495
Bb Dell <brian@oceanreefwa.com>
2019-02-11 21:31:58 +0800

    Added some extra test data.

--- demong-testdata.sql
+++ demong-testdata.sql
@@ -5,7 +5,12 @@ INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Bugs", "Bunny", 
 INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Yosemite", "Sam", "Warner", "", "Mr");
 INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Road","Runner","Warner","Bird Brain","");
 INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Wile E","Coyote","Warner","","Mr");
-INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Fred","Flinstone","Hanna Barbera","Rocky","Mr");
-INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Woody","Woodpecker","Walter Lantz","","");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Fred","Flintstone","Hanna Barbera","Rocky","Mr");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Popeye","","Famous Studios - Paramount","The Sailorman","Mr");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Betty","Boop","Fleischer","","Miss");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Winnie","the Pooh","Disney","","Pooh Bear");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Yogi","Bear","Hanna Barbera","","Mr");
+INSERT INTO person (fname, lname, mname, alias, title) VALUES ("Porky","Pig","Warner","","Mr");
+
 
 SELECT * from person;
